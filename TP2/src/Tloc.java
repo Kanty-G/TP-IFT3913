eb590a3fae7c472617eba0d302dae1a764bccceb
Kanty Gakima <91140943+gakkal29@users.noreply.github.com>
2023-10-29 11:16:08 -0400

    Update Tloc.java

--- TP2/src/Tloc.java
+++ TP2/src/Tloc.java
@@ -2,65 +2,54 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Scanner;
 
+//légère modification de TLOC du TP1 pour qu'on puisse lui donner en paramètre un repertoire et il renvoie le 
+//total de lignes de codes des fichiers tests
 public class Tloc {
-   //Il suffit de juste faire la soustraction de LOC et CLOC ou réustiliser code premier TP de TLOC
-    public static int calculateNCLOC(String filePath) throws IOException {
 
+    public static int calculateTLOC(File file) throws IOException {
         int tloc = 0;
 
-        //we open the file in read mode
-        FileReader fileReader = new FileReader(filePath);
-        BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-        String line;
-        while ((line = bufferedReader.readLine()) != null) {
-            //verify if the line does not start with characters that starts 
-            if (!line.isEmpty() && !line.trim().startsWith("/") && !line.trim().startsWith("*")) {
-                tloc++;
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    tloc += calculateTLOC(f);
+                }
             }
+        } else if (file.getName().toLowerCase().contains("test") && file.getName().endsWith(".java")) {
+
+            FileReader fileReader = new FileReader(file);
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                // Verify if the line does not start with comment characters
+                if (!line.trim().startsWith("//") && !line.trim().startsWith("/*") && !line.trim().startsWith("*")) {
+                    tloc++;
+                }
+            }
+            bufferedReader.close();
+            fileReader.close();
         }
-        //we close the file
-        bufferedReader.close();
-        fileReader.close();
+
         return tloc;
     }
-    
-    /**
-     * 
-     * @param path: the input folder path
-     * @return : boolean result of the validity
-     */
-    static boolean isValid(String[] path){
-        Boolean valid = false;
-        //we have to verify if the path entry is a java file
-        try{ 
-            File fileEntry   = new File(path[0]);
-            Boolean hasPath   = (path.length == 1);
-            Boolean isFile     = fileEntry.isFile();
-            Boolean isJavaType = fileEntry.getName().contains(".java");
-            valid      = hasPath && isFile && isJavaType;
-        } catch (Exception e) {}
-        return valid;
-    }
-    public static void main(String[] args) throws Exception {
-        try {
-            Scanner scanner = new Scanner(System.in);
-            Boolean hasArgs = true;
-            String[] entries = new String[1];
-            Boolean isValid = isValid(args);
-            while(!isValid) {
-                hasArgs = false;
-                System.out.println("Entrez un chemin de fichier Java valide:");
-                entries[0] = scanner.nextLine();
-                isValid    = isValid(entries);
-            }
-            scanner.close();
-            String filePath = (hasArgs)? args[0]: entries[0];
-            int tloc  = calculateNCLOC(filePath);
-            System.out.println(tloc);
-        } catch (Exception e) {e.printStackTrace();}       
-    }
 
+    public static void main(String[] args) throws Exception {
+        if (args.length != 1) {
+            System.out.println("Usage: java Tloc <directory_path>");
+            return;
+        }
+
+        String directoryPath = args[0];
+        File directory = new File(directoryPath);
+
+        if (!directory.exists() || !directory.isDirectory()) {
+            System.out.println("Invalid directory path");
+            return;
+        }
+
+        int tloc = calculateTLOC(directory);
+        System.out.println("TLOC: " + tloc);
+    }
 }
